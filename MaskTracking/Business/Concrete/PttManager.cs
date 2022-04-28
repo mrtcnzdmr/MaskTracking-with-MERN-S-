d115032080a89cd4490ad54f515943249955103e
mrtcnzdmr <ozdemirmert95@gmail.com>
2022-04-28 17:19:43 +0300

    MaskTracking(with MERNİS)
    MaskTracking(with MERNİS)

---
+++ MaskTracking/Business/Concrete/PttManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager:ISupplierServise
+    {
+        private IApplicantServise _applicantServise;
+
+        public PttManager(IApplicantServise applicantServise) //Constructor 
+        {
+            _applicantServise = applicantServise;
+        }
+        public void GiveMask(Person person)
+        {
+            
+            if (_applicantServise.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ");
+            }
+        }
+    }
+}
---
+++ MaskTracking/WorkAround/Program.cs
@@ -0,0 +1,31 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace WorkAround
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person person1 = new Person();
+            person1.FirstName = "Ahmet";
+            person1.LastName = "Deneme";
+            person1.DateofBirthYear = 22-02-2000;
+            person1.NationalIdentity = 11112222331;
+
+            
+           
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+        }
+        //Resharper
+        
+
+    }
+    
+}
